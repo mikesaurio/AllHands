b4f78645edd531e4619bfe51ade5172a21b5817c
mikesaurio <mikesaurio@gmail.com>
2015-10-18 16:06:59 -0500

    te permite crear un allhands o unirte a uno

--- app/controllers/allhands_controller.rb
+++ app/controllers/allhands_controller.rb
@@ -1,6 +1,10 @@
 class AllhandsController < ApplicationController
-  def index
+  before_action :set_user
 
+  def index
+    if params[:call]
+      validate(params)
+    end
   end
 
   def show
@@ -9,22 +13,31 @@ class AllhandsController < ApplicationController
 
   def create
 	unless params[:key_call].blank?
-    
-    if Call.where(id_admin: current_user.id, is_active: true).blank?
-      Call.create(id_call: params[:key_call],id_admin: current_user.id, is_active: true)
+    call = Call.where(id_admin: current_user.id, is_active: true)
+    if call.blank?
+      call_id = Call.create(id_call: params[:key_call],id_admin: current_user.id, is_active: true)
     else
-      
+      call_id = call.last
     end
-		
-		render :show
+    redirect_to allhand_path(call_id.id), notice: 'Bienvenido al AllHands.'
 	end
   end
 
-  def validate
-  	render :show
+  def validate(params)
+      call_key = params[:call][:key]
+      puts '***********************'
+      call = Call.where(id_call: call_key, is_active: true)
+      unless call.blank?
+        redirect_to allhand_path(call.last.id), notice: 'Bienvenido al AllHands.'
+      else
+        redirect_to allhands_path, error: 'No existe ninguna llamada con este ID.'
+      end
   end
 
-  private
+  private 
+  def set_user
+    @current_user = current_user || nil
+  end
 
 
 end
--- app/helpers/allhands_helper.rb
+++ app/helpers/allhands_helper.rb
@@ -1,8 +1,8 @@
 module AllhandsHelper
 	
-  def create_key
-    unless Call.where(id_admin: current_user.id, is_active: true).blank?
-      Call.where(id_call: params[:key_call],id_admin: current_user.id, is_active: true).last
+  def create_key(current_user_id)
+    unless Call.where(id_admin: current_user_id, is_active: true).blank?
+      Call.where(id_admin: current_user_id, is_active: true).last.id_call
     else
     	(0...25).map { (65 + rand(26)).chr }.join
     end
